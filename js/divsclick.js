7b2971dc8e0afdd8fb1a2b88526b52b072f78f29
Leonard Monteiro <leonardommonteiro2004@gmail.com>
2024-05-26 21:40:38 -0300

    aaa

---
+++ js/divsclick.js
@@ -0,0 +1,32 @@
+// script.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    const divcp = document.getElementById('carnepanela');
+
+    divcp.addEventListener('click', () => {
+        window.location.href = 'receitas/carnepanela.html';
+    });
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+    const divst = document.getElementById('strogonoff');
+
+    divst.addEventListener('click', () => {
+        window.location.href = 'receitas/strogonoff.html';
+    });
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+    const divst = document.getElementById('lasanha');
+
+    divst.addEventListener('click', () => {
+        window.location.href = 'receitas/lasanha.html';
+    });
+});
+
+
+
+
+
+
+
+
+/*const divla= document.getElementById("lasanha")   
+ const divst= document.getElementById("strogonoff")   */
